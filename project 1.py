6efda66eaf945f08fc76db6cda6bd4b9b141241d
yuvamithraa <160617025+yuvamithraa@users.noreply.github.com>
2024-03-04 20:12:27 +0530

    Add files via upload

---
+++ project 1.py
@@ -0,0 +1,14 @@
+#c=(fah-32)/1.8
+#Fahrenhei to celcius
+# fahrenheit=float(input("enter the temperature:"))
+# celcius=(fahrenheit-32)/1.8
+# celcius=round(celcius,2)
+# print(f"we have converted {fahrenheit } to { celcius}")
+
+
+
+
+
+
+
+
